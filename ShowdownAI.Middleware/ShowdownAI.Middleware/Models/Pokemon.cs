8ac5c3fb774f4abb59ff84af5577c87a0dc361bd
Nathan Cullen <45045903+NathanJCullen@users.noreply.github.com>
2024-07-15 16:18:58 +0100

    alphabetise Pokemon class properties

--- ShowdownAI.Middleware/ShowdownAI.Middleware/Models/Pokemon.cs
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Models/Pokemon.cs
@@ -2,20 +2,23 @@
 {
     public class Pokemon
     {
-        public string Ident { get; set; }
-        public string Details { get; set; }
-        public string Condition { get; set; }
-        public bool Active { get; set; }
-        public Stats Stats { get; set; } 
-        public List<string> Moves { get; set; }
-        public string BaseAbility { get; set; }
-        public string Item { get; set; }
-        public string Pokeball { get; set; }
         public string Ability { get; set; }
+        public bool Active { get; set; }
+        public string BaseAbility { get; set; }
         public bool Commanding { get; set; }
+        public string Condition { get; set; }
+        public string Details { get; set; }
+        public string Ident { get; set; }
+        public string Item { get; set; }
+        public List<string> Moves { get; set; }
+        public string Pokeball { get; set; }
         public bool Reviving { get; set; }
-        public string TeraType { get; set; }
+        public Stats Stats { get; set; } 
+        public Status Status {get ; set;}
         public string Terastallized { get; set; }
+        public string TeraType { get; set; }
+
+    }
 
     }
 }
