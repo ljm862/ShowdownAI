c1184c8b2aba5f11e1726ebe9ff260affb92aca6
Nathan Cullen <45045903+NathanJCullen@users.noreply.github.com>
2024-07-24 13:13:56 +0100

    export all helpers from util.js

---
+++ utils/utils.js
@@ -0,0 +1,9 @@
+const { clickButton } = require("./clickButton");
+const { getPageByUrl } = require("./getPageByUrl");
+const { inputText } = require("./inputText");
+
+module.exports = {
+  clickButton,
+  getPageByUrl,
+  inputText,
+};
