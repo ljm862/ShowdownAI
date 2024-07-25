84515b150d97898d69a04d73af45fd7efaf2a2d6
benphillips1997 <ben.phillips1997@hotmail.com>
2024-07-25 23:04:36 +0100

    Added enum for move target type

---
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Models/TargetType.cs
@@ -0,0 +1,27 @@
+﻿namespace ShowdownAI.Middleware.Models
+{
+    public enum TargetType
+    {
+        None,
+
+        // single-target
+        normal,
+        any,
+        adjacentAlly,
+        adjacentFoe,
+        adjacentAllyOrSelf,
+
+        // single-target, automatic
+        self,
+        randomNormal,
+
+        // spread
+	    allAdjacent,
+        allAdjacentFoes,
+
+        // side and field
+        allySide,
+        foeSide,
+        all 
+    }
+}
