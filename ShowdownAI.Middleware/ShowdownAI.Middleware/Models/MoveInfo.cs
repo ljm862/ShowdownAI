e804479cfe806ce642d85c8bc6a79b163972d427
Lmorgan89 <billsmith116@gmail.com>
2024-07-30 18:35:21 +0200

    Initial skeleton for picking a move
    Runs a damage calculation and picks the one that deals the most damage for now.

--- ShowdownAI.Middleware/ShowdownAI.Middleware/Models/MoveInfo.cs
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Models/MoveInfo.cs
@@ -2,15 +2,18 @@
 {
     public class MoveInfo
     {
+        public bool Disabled { get; set; }
+        public string Id { get; set; }
+        public AttackMethod Method { get; set; }
+        public int Maxpp { get; set; }
         /// <summary>
         /// Name of the move
         /// </summary>
         public string Move { get; set; }
-        public string Id { get; set; }
+        public int Power { get; set; }
         public int Pp { get; set; }
-        public int Maxpp { get; set; }
         public string Target { get; set; }
         public TargetType TargetType { get; set; }
-        public bool Disabled { get; set; }
+        public TypeName TypeName { get; set; }
     }
 }
---
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Services/BattleTracker.cs
@@ -0,0 +1,26 @@
+﻿using ShowdownAI.Middleware.Models;
+
+namespace ShowdownAI.Middleware.Services
+{
+    /// <summary>
+    /// Acts as the memory part of the brain. Remembers which pokemon are on the field, which pokemon the opponent has that it has seen, items, moves, abilities, etc.
+    /// </summary>
+    public class BattleTracker
+    {
+        public List<Pokemon> OurTeam;
+        public List<Pokemon> TheirTeam;
+
+        public Pokemon OurCurrentPokemon;
+        public Pokemon TheirCurrentPokemon;
+
+        public ActivePokemon OurActivePokemon;
+
+        public BattleTracker()
+        {
+            OurTeam = new();
+            TheirTeam = new();
+            OurCurrentPokemon = new();
+            TheirCurrentPokemon = new();
+        }
+    }
+}
