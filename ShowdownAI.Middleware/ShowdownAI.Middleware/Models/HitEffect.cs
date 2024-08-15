3e86a21e2246f26950e05fe9f45f45e6073b80c3
benphillips1997 <ben.phillips1997@hotmail.com>
2024-08-15 23:04:43 +0100

    Added boosts, hitEffect, seconaryEffect and moveFlags for MoveData properties

---
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Models/HitEffect.cs
@@ -0,0 +1,19 @@
+﻿namespace ShowdownAI.Middleware.Models
+{
+    public class HitEffect
+    {
+        // set pokemon conditions
+        //public ??? Boosts? { get; set; }
+        public string? Status { get; set; }
+        public string? VolatileStatus { get; set; }
+
+        // set side/slot conditions
+        public string? SideCondition { get; set; }
+        public string? SlotCondition { get; set; }
+
+        // set field condition
+        public string? PseudoWeather { get; set; }
+        public string? Terrain { get; set; }
+        public string? Weather { get; set; }
+    }
+}
---
+++ ShowdownAI.Middleware/ShowdownAI.Middleware/Models/SecondaryEffect.cs
@@ -0,0 +1,13 @@
+﻿namespace ShowdownAI.Middleware.Models
+{
+    public class SecondaryEffect
+    {
+        public Boosts Boosts { get; set; }
+        public int? Chance { get; set; }
+        // needs ability implementation
+        //public Ability Ability { get; set; }   // Used to flag a secondary effect as added by Poison Touch
+        public bool? DustProof { get; set; }    // Applies to Sparkling Aria's secondary effect: Affected by Sheer Force but not Shield Dust.
+        public bool? Kingsrock { get; set; }    // Gen 2 specific mechanics: Bypasses Substitute only on Twineedle, and allows it to flinch sleeping/frozen targets
+        public HitEffect? Self { get; set; }
+    }
+}
